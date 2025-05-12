941120153e53ce8b6a46da630f0e417e57e02507
theo-ooooo <kkw.theo@gmail.com>
2025-05-12 11:47:01 +0900

    refactor: querykey 모듈화

--- src/features/categories/hooks/useCategory.ts
+++ src/features/categories/hooks/useCategory.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { getCategory } from '../api/categoiresApi'
+import categoriesKey from '@/lib/constants/queryKeys/categories'
 
 export const useCategory = ({ categoryId }: { categoryId: number }) => {
   return useQuery({
-    queryKey: [`categories/${categoryId}`],
+    queryKey: categoriesKey.detail(categoryId),
     queryFn: () => getCategory({ categoryId }),
     staleTime: 1000 * 60 * 5,
   })
